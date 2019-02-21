494ac219ed910c53df822961a21f5eb6c8c0ebd6
Joel Corey <joelcorey@fastmail.com>
2019-02-21 14:22:28 -0800

    loaf add

--- object-drills-checkpoint-9.js
+++ object-drills-checkpoint-9.js
@@ -1,4 +1,14 @@
 'use strict';
 //https://courses.thinkful.com/ei-js-v1/checkpoint/9
 
+let loaf = {
+  flour: 300,
+  water: 210,
+  hydration: function(){
+    return this.water / this.flour * 100;
+  }
+};
 
+console.log(loaf.flour);
+console.log(loaf.water);
+console.log(loaf.hydration());
