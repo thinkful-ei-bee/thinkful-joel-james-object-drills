fd539c503cc6410e4ea846e6b3972c2160bd0e5f
Joel Corey <joelcorey@fastmail.com>
2019-02-21 14:31:10 -0800

    Itterate over value and property

--- object-drills-checkpoint-9.js
+++ object-drills-checkpoint-9.js
@@ -1,6 +1,7 @@
 'use strict';
 //https://courses.thinkful.com/ei-js-v1/checkpoint/9
 
+// 1:
 let loaf = {
   flour: 300,
   water: 210,
@@ -9,6 +10,21 @@ let loaf = {
   }
 };
 
-console.log(loaf.flour);
-console.log(loaf.water);
-console.log(loaf.hydration());
+// console.log(loaf.flour);
+// console.log(loaf.water);
+// console.log(loaf.hydration());
+
+// 2:
+let derp = {
+  foo: 'this',
+  bar: 'that',
+  fum: 'not this',
+  quux: 'not this but that',
+  spam: 'waffle',
+};
+
+// for (const prop in derp) {
+//   console.log(`Property name: ${prop}, property value: ${derp[prop]}`);
+// }
+
+// 3:
