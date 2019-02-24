80208554b624774165f4ddc5eb42d58334ae0b47
Joel Corey <joelcorey@fastmail.com>
2019-02-24 09:05:14 -0800

    Object cipher fixed

--- object-drills-checkpoint-9.js
+++ object-drills-checkpoint-9.js
@@ -73,37 +73,45 @@ people.map((people) => {
   }
 });
 
-console.log(people);
-
-people.forEach(person => {
-  let noBoss = `${person.jobTitle} ${person.name} doesn't report to anybody.`;
-  let hasBoss = `${person.jobTitle} ${person.name} reports to ${person.boss}`;
-  person.boss === undefined ? console.log(noBoss) : console.log(hasBoss);
-});
+//console.log(people);
+// people.forEach(person => {
+//   let noBoss = `${person.jobTitle} ${person.name} doesn't report to anybody.`;
+//   let hasBoss = `${person.jobTitle} ${person.name} reports to ${person.boss}`;
+//   person.boss === undefined ? console.log(noBoss) : console.log(hasBoss);
+// });
 
 // 6: Redo of cracking the code
 
 function decode(word, cipher){
-  if(cipher[word[0]]){
-    return word[cipher[word[0]]];
+  for (var key in cipher) {
+    if (key === word[0]) {
+      return word[cipher[key] - 1];
+    }
+    //return key === word[0] ? word[cipher[key] - 1] : ' ';
   }
-  return 'No match';
+  return ' ';
+}
+
+function decodeWords(input, cipher){
+  let message = '';
+  for (let i = 0; i < input.length; i++) {  
+    message += decode(input[i], cipher);
+  }
+  return message;  
 }
 
 let input = 'craft block argon meter bells brown croon droop'.split(' ');
+let cipher = {
+  a: 2,
+  b: 3,
+  c: 4,
+  d: 5,
+};
+console.log(decodeWords(input, cipher));
 
-
-function decodeWords(words){
-  let cipher = {
-    a: 2,
-    b: 3,
-    c: 4,
-    d: 5,
-  };
-  for (const word in words) {
-    console.log(decode(input[0], cipher));
-  }
-  
-}
-
-decodeWords(input);
+// let obj = {
+//   a: 'some value',
+//   b: 'another thing'
+// };
+// let filtered = ['a'].filter(key => obj.hasOwnProperty(key));
+// console.log(obj[filtered]);
